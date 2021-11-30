897312f18dbe7779b81f1f40473eb7f7e5fbd9ee
msk- <mattkingston@gmail.com>
2021-11-30 11:10:42 +0000

    Flatten

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import api from './api';
+import reporting from './reporting';
+import settlement from './settlement';
+import ledger from './ledger';
+// Don't export everything from here because not everything is meant to be exported
+export {
+  ResponseKind,
+  MlApiResponse,
+  ApiError,
+  Options,
+  OptionsOfThrowMlError,
+  defaultOpts,
+} from './shared';
+import * as types from './types';
+
 export default {
-  api
+  types,
+  ledger,
+  settlement,
+  reporting,
 }
